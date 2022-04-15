204eee2e86572c4c63c3f2a915ed9543daed05d2
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2022-04-15 14:57:52 +0200

    Update main.py
    Fully rewritten. Cant even list all changes.

--- main.py
+++ main.py
@@ -1,121 +1,108 @@
-import json
-from json import JSONEncoder
-from waitress import serve
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, jsonify
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
+##############################################
+# Bunkercoin REST API
+# Originally made by IdotMaster1
+# Fully rewritten by BastelPichi.
+# This code is distributed under GPL3 license.
+# See LICENSE file for licensing.
+#
+# Todo: add exceptions when communication
+# with bunkercoind
+#
+# Use a proper WSGI Server
+# (e.g. Apache or NGINX) to deploy
+##############################################
+
+username = "rpcuser"
+password = "rpcpassword"
+
+
+apiversion = 2.0
 app = Flask(__name__)
-api = Api(app)
-
-apiversion = "1.0"
 
 
-class HelloWorld(Resource):
-    print("Hello World")
-    def get(self):
-        return {'api': 'online'}
-    pass
+@app.route("/apiinfo")
+def apiinfo():
+    return jsonify({
+        "apiver": apiversion,
+        "result": {
+            "id": "bkc-rest-api",
+            "version": apiversion,
+            "description": "Bunkercoin REST API Alpha 2.0",
+            "author": "BastelPichi"
+        }
+    })
 
-class apiinfo(Resource):
-    print("Hello World")
-    def get(self):
-     return {
-         'apiver': apiversion,
-         'result': {
-                'id': 'bkc-rest-api',
-                'version': apiversion,
-                'description': 'Bunkercoin REST API Alpha 0.1',
-                'author': 'IdotMaster1'
-         }
-         } # This will print the balance
-    pass
+@app.route("/blockcount")
+def blockcount():
+    rpc_connection = AuthServiceProxy(f"http://{username}:{password}@127.0.0.1:22225")
+    result = rpc_connection.getblockcount()
+    return jsonify({
+        "apiver": apiversion,
+        "result": {
+            "blockcount": result
+        }
+    })
 
-class bloc(Resource):
-    print("Hello World")
-    def get(self):
-     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:22225" % ('user', 'pass'))
-     get = rpc_connection.getblockcount()
-     return {
-         'apiver': apiversion,
-         'result': {
-                {"blockcount": str(get)}
-         }
-         } # This will print the balance
-    pass
-
-class hashrate(Resource):
-    print("Hello World")
-    def get(self):
-     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:22225" % ('user', 'pass'))
-     get = rpc_connection.getnetworkhashps()
-     return {
-         'apiver': apiversion,
-         'result': {
-                {"hashrate": str(get)}
-         }
-         } # This will print the balance
-    pass
-
-class diff(Resource):
-    print("Hello World")
-    def get(self):
-     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:22225" % ('user', 'pass'))
-     get = rpc_connection.getdifficulty()
-     return {
-         'apiver': apiversion,
-         'result': {
-                 {"difficulty": str(get)}
-         }
-         } # This will print the balance
-    pass
-
-class mempoolinfo(Resource):
-    print("Hello World")
-    def get(self):
-     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:22225" % ('user', 'pass'))
-     get = rpc_connection.getmempoolinfo()
-     return {
-         'apiver': apiversion,
-         'result': 
-         {
-             {"mempool": str(get)}
-         }
-     }
-          # This will print the balance
-    pass
-
-class chaininfo(Resource):
-    print("Hello World")
-    def get(self):
-     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:22225" % ('user', 'pass'))
-     get = rpc_connection.getinfo()
-     return {
-         'apiver': apiversion,
-         'result': {"info": str(get)}
-     }
-          # This will print the balance
-    pass
-class blockchaininfo(Resource):
-    print("Hello World")
-    def get(self):
-     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:22225" % ('user', 'pass'))
-     get = rpc_connection.getblockchaininfo()
-     return {
-         'apiver': apiversion,
-         'result': {"info": str(get)}
-     }
-          # This will print the balance
-    pass
-
-api.add_resource(bloc, '/blockcount')  # 
-api.add_resource(apiinfo, '/apinfo')  # 
-api.add_resource(hashrate, '/hashrate')  # 
-api.add_resource(diff, '/diff')  # 
-api.add_resource(mempoolinfo, '/meminfo')  # 
-api.add_resource(chaininfo, '/info')  # 
-api.add_resource(blockchaininfo, '/blockchaininfo')  #
+@app.route("/hashrate")
+def hashrate():
+    rpc_connection = AuthServiceProxy(f"http://{username}:{password}@127.0.0.1:22225")
+    result = rpc_connection.getnetworkhashps()
+    return jsonify({
+        "apiver": apiversion,
+        "result": {
+            "hashrate": result
+        }
+    })
 
 
-if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=5000)  # run our Flask app
+@app.route("/diff")
+def diff():
+    rpc_connection = AuthServiceProxy(f"http://{username}:{password}@127.0.0.1:22225")
+    result = rpc_connection.getdifficulty()
+    return jsonify({
+        "apiver": apiversion,
+        "result": {
+            "difficulty": result
+        }
+    })
+
+@app.route("/meminfo")
+def meminfo():
+    rpc_connection = AuthServiceProxy(f"http://{username}:{password}@127.0.0.1:22225")
+    result = rpc_connection.getmempoolinfo()
+    return jsonify({
+        "apiver": apiversion,
+        "result": {
+            "mempool": result
+        }
+    })
+
+@app.route("/info")
+def info():
+    rpc_connection = AuthServiceProxy(f"http://{username}:{password}@127.0.0.1:22225")
+    result = rpc_connection.getinfo()
+    return jsonify({
+        "apiver": apiversion,
+        "result": {
+            "info": result
+        }
+    })
+
+
+@app.route("/blockchaininfo")
+def blockchaininfo():
+    rpc_connection = AuthServiceProxy(f"http://{username}:{password}@127.0.0.1:22225")
+    result = rpc_connection.getblockchaininfo()
+    return jsonify({
+        "apiver": apiversion,
+        "result": {
+            "info": result
+        }
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
